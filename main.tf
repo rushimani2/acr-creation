aa4c0aa1bce3bb59ff6a3fb018e4ffa3630a7d0e
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2025-04-20 02:15:53 +0530

    first-commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  subscription_id = "cfe6a4c8-dde8-452b-9198-30f409fd9c67"
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "github-workflow"
+  location = "East US"
+}
+
+resource "azurerm_container_registry" "example" {
+  name                = "exampleacrregistry123"  # globally unique
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
