c20b160091b949cdeb3aa7fc4915000a9a1764bd
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2025-04-20 13:14:57 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
+output "acr_username" {
+  value     = azurerm_container_registry.acr.admin_username
+  sensitive = true  # Ensure that the username is treated as sensitive
+}
+
+output "acr_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true  # Ensure that the password is treated as sensitive
+}
+
 output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
+  value     = azurerm_container_registry.acr.login_server
+  sensitive = false  # This is not sensitive, so we can safely output it
 }
